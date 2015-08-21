d1d4df6829b91fe45724fd6e74c67be7a2a87976
Tonlin <448133553@qq.com>
2015-08-21 18:02:52 +0800

    修改启动页

--- app/src/main/java/net/oschina/app/v2/activity/SplashActivity.java
+++ app/src/main/java/net/oschina/app/v2/activity/SplashActivity.java
@@ -12,6 +12,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -38,7 +39,6 @@ public class SplashActivity extends Activity {
 
 	private static final String SPLASH_SCREEN = "SplashScreen";
 	public static final int MAX_WATTING_TIME = 3000;// 停留时间3秒
-	protected Version mVersion;
 	protected boolean mShouldGoTo = true;
 
 	@Override
@@ -46,10 +46,6 @@ public class SplashActivity extends Activity {
 		super.onResume();
 		MobclickAgent.onPageStart(SPLASH_SCREEN); // 统计页面
 		MobclickAgent.onResume(this); // 统计时长
-
-		if (!mShouldGoTo) {
-			redirectTo();
-		}
 	}
 
 	@Override
@@ -66,54 +62,47 @@ public class SplashActivity extends Activity {
 			redirectTo();
 			return;
 		}
-		AppContext.requestDailyEnglish();
+
 		//checkUpdate();
 		final View view = View.inflate(this, R.layout.v2_activity_splash, null);
 		setContentView(view);
 
+		// 渐变展示启动屏
+//		AlphaAnimation aa = new AlphaAnimation(1f, 1.0f);
+//		aa.setDuration(MAX_WATTING_TIME);
+//		view.startAnimation(aa);
+//		aa.setAnimationListener(new AnimationListener() {
+//
+//			@Override
+//			public void onAnimationEnd(Animation arg0) {
+//				if (mShouldGoTo) {
+//					redirectTo();
+//				}
+//			}
+//
+//			@Override
+//			public void onAnimationRepeat(Animation animation) {
+//			}
+//
+//			@Override
+//			public void onAnimationStart(Animation animation) {
+//			}
+//		});
+
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				redirectTo();
+			}
+		},MAX_WATTING_TIME);
+
+		AppContext.requestDailyEnglish();
 		DailyEnglish de = AppContext.getDailyEnglish();
 		if (de != null) {
 			TextView tvContent = (TextView) findViewById(R.id.tv_eng);
 			tvContent.setText(de.getContent());
 			TextView tvNote = (TextView) findViewById(R.id.tv_note);
 			tvNote.setText(de.getNote());
-		}
-
-		// 渐变展示启动屏
-		AlphaAnimation aa = new AlphaAnimation(0.3f, 1.0f);
-		aa.setDuration(2500);
-		view.startAnimation(aa);
-		aa.setAnimationListener(new AnimationListener() {
-
-			@Override
-			public void onAnimationEnd(Animation arg0) {
-				if (mShouldGoTo) {
-					redirectTo();
-				}
-			}
-
-			@Override
-			public void onAnimationRepeat(Animation animation) {
-			}
-
-			@Override
-			public void onAnimationStart(Animation animation) {
-			}
-		});
-
-		// 兼容低版本cookie（1.5版本以下，包括1.5.0,1.5.1）
-		AppContext appContext = (AppContext) getApplication();
-		String cookie = appContext.getProperty("cookie");
-		if (StringUtils.isEmpty(cookie)) {
-			String cookie_name = appContext.getProperty("cookie_name");
-			String cookie_value = appContext.getProperty("cookie_value");
-			if (!StringUtils.isEmpty(cookie_name)
-					&& !StringUtils.isEmpty(cookie_value)) {
-				cookie = cookie_name + "=" + cookie_value;
-				appContext.setProperty("cookie", cookie);
-				appContext.removeProperty("cookie_domain", "cookie_name",
-						"cookie_value", "cookie_version", "cookie_path");
-			}
 		}
 
 		View root = findViewById(R.id.app_start_view);
@@ -139,37 +128,8 @@ public class SplashActivity extends Activity {
 		win.setAttributes(winParams);
 	}
 
-	private void checkUpdate() {
-		UmengUpdateAgent.setUpdateListener(new UmengUpdateListener() {
-
-			@Override
-			public void onUpdateReturned(int updateStatus,
-					UpdateResponse updateInfo) {
-				switch (updateStatus) {
-				case UpdateStatus.Yes: // has update
-					// mVersion = new Version(updateInfo);
-					mShouldGoTo = false;
-					UmengUpdateAgent.showUpdateDialog(getApplicationContext(),
-							updateInfo);
-					break;
-				case UpdateStatus.No: // has no update
-					break;
-				case UpdateStatus.NoneWifi: // none wifi
-					break;
-				case UpdateStatus.Timeout: // time out
-					break;
-				}
-			}
-		});
-		UmengUpdateAgent.setUpdateAutoPopup(false);
-		UmengUpdateAgent.update(getApplicationContext());
-	}
-
 	private void redirectTo() {
 		Intent intent = new Intent(this, MainActivity.class);
-		// if (mVersion != null) {
-		// intent.putExtra(Version.BUNDLE_KEY_VERSION, mVersion);
-		// }
 		startActivity(intent);
 		finish();
 	}
